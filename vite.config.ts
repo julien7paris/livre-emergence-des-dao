9354e686c190159e5d82b67c64b330d901b454b8
julien7paris <julien.lallemand92@gmail.com>
2025-08-24 15:30:43 +0200

    Build + config GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/livre-emergence-des-dao/',   // ⬅important pour GitHub Pages
 })
